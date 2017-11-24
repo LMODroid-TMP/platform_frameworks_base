35293fd0e3ab182cb66c4d053d9cae705004e5a5
Simon Shields <simon@lineageos.org>
2017-11-24 16:05:19 +1100

    PhoneWindowManager: add VolumeKeyHandler
    We can't use a generic DeviceKeyHandler here, because we don't want to
    override some of the AOSP logic (silencing incoming calls, screenshots)
    and we need to know if the device is interactive or not (for screen-off
    music controls)
    
    Change-Id: I485c2f6006c5bbd358ba0cbd32917689826c5c6d

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -249,6 +249,7 @@ import dalvik.system.PathClassLoader;
 
 import com.libremobileos.providers.LMOSettings;
 import com.libremobileos.util.ActionUtils;
+import com.libremobileos.util.VolumeKeyHandler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -705,6 +706,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mKeyguardDrawnTimeout = 1000;
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
+
+    private VolumeKeyHandler mVolumeKeyHandler;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -4707,6 +4710,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // {@link interceptKeyBeforeDispatching()}.
                     result |= ACTION_PASS_TO_USER;
                 } else if ((result & ACTION_PASS_TO_USER) == 0) {
+                    if (mVolumeKeyHandler.handleVolumeKey(event, interactive)) {
+                        break;
+                    }
                     // If we aren't passing to the user and no one else
                     // handled it send it to the session manager to
                     // figure out.
@@ -5949,6 +5955,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mKeyguardDelegate.onBootCompleted();
             }
         }
+        mVolumeKeyHandler = new VolumeKeyHandler(mContext);
         mSideFpsEventHandler.onFingerprintSensorReady();
         startedWakingUp(Display.DEFAULT_DISPLAY_GROUP, PowerManager.WAKE_REASON_UNKNOWN);
         finishedWakingUp(Display.DEFAULT_DISPLAY_GROUP, PowerManager.WAKE_REASON_UNKNOWN);
