285ec7f7f89265359acb17144085d8efb26057a1
nift4 <nift4@protonmail.com>
2023-04-29 14:53:56 +0200

    WindowManager: finally proper desktop mode handling
    * The previous approach turned out to break Miracast screen mirroring,
      because of the windowing mode.
    * Instead of hardcoding desktop, use FLAG_SHOULD_SHOW_SYSTEM_DECORATIONS
      to detect our custom display, as it's impossible for that flag to be
      set on a non-default display on AOSP.
    * Make InputManagerCallback check desktop mode per-display, to match
      new behaviour.
    
    Change-Id: I4a701a2cb219e49d3f89aac35720a3ea5e37ec52

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -4242,7 +4242,8 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
     }
 
     boolean forceDesktopMode() {
-        return mWmService.mForceDesktopModeOnExternalDisplays && !isDefaultDisplay && !isPrivate();
+        return ("VNC".equals(mDisplay.getName()) || mWmService.mForceDesktopModeOnExternalDisplays)
+            && !isDefaultDisplay && !isPrivate();
     }
 
     /** @see WindowManagerInternal#onToggleImeRequested */
