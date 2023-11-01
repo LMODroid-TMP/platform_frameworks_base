cc0ee456cad0507a94304d9e50f264f23b43ecb6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-01 20:42:48 +0530

    SystemUI: Enable blur behind the power menu
    Change-Id: I01e7d2802e0a9a304d96c68663d7564bb104e1e5

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -143,6 +143,7 @@ import com.android.systemui.plugins.GlobalActionsPanelPlugin;
 import com.android.systemui.scrim.ScrimDrawable;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.shade.ShadeController;
+import com.android.systemui.statusbar.BlurUtils;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
 import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.phone.CentralSurfaces;
@@ -280,6 +281,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
     private final DialogLaunchAnimator mDialogLaunchAnimator;
     private final ControlsComponent mControlsComponent;
+    private final BlurUtils mBlurUtils;
 
     @VisibleForTesting
     public enum GlobalActionsEvent implements UiEventLogger.UiEventEnum {
@@ -390,7 +392,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             ShadeController shadeController,
             KeyguardUpdateMonitor keyguardUpdateMonitor,
             DialogLaunchAnimator dialogLaunchAnimator,
-            ControlsComponent controlsComponent) {
+            ControlsComponent controlsComponent,
+            BlurUtils blurUtils) {
         mContext = context;
         mWindowManagerFuncs = windowManagerFuncs;
         mAudioManager = audioManager;
@@ -426,6 +429,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         mKeyguardUpdateMonitor = keyguardUpdateMonitor;
         mDialogLaunchAnimator = dialogLaunchAnimator;
         mLineageGlobalActions = LineageGlobalActions.getInstance(mContext);
+        mBlurUtils = blurUtils;
 
         // receive broadcasts
         IntentFilter filter = new IntentFilter();
@@ -787,7 +791,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 mCentralSurfacesOptional,
                 mShadeController,
                 mKeyguardUpdateMonitor,
-                mLockPatternUtils);
+                mLockPatternUtils, mBlurUtils);
         dialog.setOnDismissListener(this);
         dialog.setOnShowListener(this);
 
@@ -2632,6 +2636,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         private final ShadeController mShadeController;
         private KeyguardUpdateMonitor mKeyguardUpdateMonitor;
         private LockPatternUtils mLockPatternUtils;
+        private BlurUtils mBlurUtils;
         private float mWindowDimAmount;
 
         protected ViewGroup mContainer;
@@ -2706,7 +2711,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 Optional<CentralSurfaces> centralSurfacesOptional,
                 ShadeController shadeController,
                 KeyguardUpdateMonitor keyguardUpdateMonitor,
-                LockPatternUtils lockPatternUtils) {
+                LockPatternUtils lockPatternUtils,
+                BlurUtils blurUtils) {
             // We set dismissOnDeviceLock to false because we have a custom broadcast receiver to
             // dismiss this dialog when the device is locked.
             super(context, themeRes, false /* dismissOnDeviceLock */);
@@ -2727,6 +2733,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             mShadeController = shadeController;
             mKeyguardUpdateMonitor = keyguardUpdateMonitor;
             mLockPatternUtils = lockPatternUtils;
+            mBlurUtils = blurUtils;
             mGestureDetector = new GestureDetector(mContext, mGestureListener);
         }
 
@@ -3023,6 +3030,11 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 float alpha = isEnter ? progress : 1 - progress;
                 mGlobalActionsLayout.setAlpha(alpha);
                 window.setDimAmount(mWindowDimAmount * alpha);
+
+                if (mBlurUtils.supportsBlursOnWindows()) {
+                    mBlurUtils.applyBlur(window.getDecorView().getViewRootImpl(),
+                             (int) mBlurUtils.blurRadiusOfRatio(progress), false);
+                }
 
                 // TODO(b/213872558): Support devices that don't have their power button on the
                 // right.
