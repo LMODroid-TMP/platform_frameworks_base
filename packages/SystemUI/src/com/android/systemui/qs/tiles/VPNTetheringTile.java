1cc37244f59bc6e1e2f2c7e6fd878cd2d3cb9a73
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-11-29 17:24:59 +0000

    fixup! SystemUI: Add VPNTethering tile
    Change-Id: Ice6a51d54acba6cb45d095546e301cd777014e22
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/VPNTetheringTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/VPNTetheringTile.java
@@ -44,6 +44,7 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.qs.SettingObserver;
@@ -66,6 +67,7 @@ public class VPNTetheringTile extends QSTileImpl<BooleanState> {
     @Inject
     public VPNTetheringTile(
             QSHost host,
+            QsEventLogger uiEventLogger,
             @Background Looper backgroundLooper,
             @Main Handler mainHandler,
             FalsingManager falsingManager,
@@ -76,7 +78,7 @@ public class VPNTetheringTile extends QSTileImpl<BooleanState> {
             UserTracker userTracker,
             SecureSettings secureSettings
     ) {
-        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+        super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
 
         mSetting = new SettingObserver(secureSettings, mHandler, Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
