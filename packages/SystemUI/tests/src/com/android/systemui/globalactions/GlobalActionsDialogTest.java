be03f8210a46cb17828f48badee4b5f8970dd4be
Dhina17 <dhinalogu@gmail.com>
2024-06-04 17:14:58 +0530

    fixup related to power menu
    Change-Id: I11ac5e6c8129bf8b95dc79af658dd22d9005a63e

--- packages/SystemUI/tests/src/com/android/systemui/globalactions/GlobalActionsDialogTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/globalactions/GlobalActionsDialogTest.java
@@ -71,7 +71,6 @@ import com.android.systemui.plugins.GlobalActionsPanelPlugin;
 import com.android.systemui.settings.UserContextProvider;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
-import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.telephony.TelephonyListenerManager;
@@ -136,7 +135,6 @@ public class GlobalActionsDialogTest extends SysuiTestCase {
     @Mock private UserTracker mUserTracker;
     @Mock private PackageManager mPackageManager;
     @Mock private SecureSettings mSecureSettings;
-    @Mock private StatusBar mStatusBar;
     @Mock private KeyguardUpdateMonitor mKeyguardUpdateMonitor;
     private ControlsComponent mControlsComponent;
 
